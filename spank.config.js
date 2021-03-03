f2cfa3533ad000cfd002c6d66ce7e6a233b1a2f8
aslak bakkeland <aslak.bakkeland@everyday.no>
2021-03-03 12:33:12 +0000

    fix: hopefully resolved build issue

---
+++ spank.config.js
@@ -0,0 +1,7 @@
+//spank.config.js
+module.exports = {
+  sitemap: 'sitemap.json',
+  entrypoint: 'build/dist/__index.html',
+  script: 'build/dist/index.js',
+  outputDir: 'build/dist'
+}
--- src/pages/index.svelte
+++ src/pages/index.svelte
@@ -1,7 +1,7 @@
 <script> 
   import Posts from '../components/posts.svelte'
   
-  let baseurl = 'https://7f4df120ff2e.ngrok.io'
+  let baseurl = 'https://93e2032cb40e.ngrok.io'
   
   async function getRandomNumber() {
     const res = await fetch(`${baseurl}/wp-json/public-woo/v1/products/`);
